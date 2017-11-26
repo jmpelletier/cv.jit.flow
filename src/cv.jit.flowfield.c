883730753e0355f4bcc05cbd21270547ff53a283
Jean-Marc Pelletier <jmp@jmpelletier.com>
2017-11-26 10:49:27 +0900

    Updated licenses

--- src/cv.jit.flowfield.c
+++ src/cv.jit.flowfield.c
@@ -4,10 +4,23 @@
 	Finds and outputs the position of strong flowfield (corners) in the input image.
 	Useful for initializing tracking algorithms.
 	
-	Copyright 2006
-	Jean-Marc Pelletier
-	jmp@iamas.ac.jp
+	Copyright (c) 2008-2017, Jean-Marc Pelletier
+	jmp@jmpelletier.com
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 
 #include "jit.common.h"
 #include "cv.h"
